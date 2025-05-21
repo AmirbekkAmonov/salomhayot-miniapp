7b42931f5eade2a31f5bb7a8c098a713c07f97f1
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-05-21 17:34:03 +0500

    telegram mavzusi

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,29 @@
-import { BrowserRouter } from "react-router-dom"
-import AppRouter from "./router"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
+import AppRouter from "./router";
+import WebApp from "@twa-dev/sdk";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
+  useEffect(() => {
+    WebApp.ready();
+
+    // Telegram ilovasining hozirgi mavzusini oling
+    const theme = WebApp.colorScheme; // 'light' yoki 'dark'
+
+    // HTML tagga data-theme atributini o'rnating
+    document.documentElement.setAttribute("data-theme", theme);
+  }, []);
+
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
         <AppRouter />
       </QueryClientProvider>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
