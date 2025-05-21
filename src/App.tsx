0fdf92fca9ca4783f0968ee2d7168552be23cf0a
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-05-21 18:58:54 +0500

    loading home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "./router";
 import WebApp from "@twa-dev/sdk";
+import Loader from "./components/ui/Loader";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    WebApp.ready(); // Telegram'ga tayyorligini bildiradi
+    document.documentElement.setAttribute("data-theme", WebApp.colorScheme);
+    // 500ms kechikish bilan loading yo‘qoladi
+    setTimeout(() => setIsReady(true), 500); 
+  }, []);
+
   useEffect(() => {
     WebApp.ready();
 
@@ -20,7 +30,7 @@ function App() {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <AppRouter />
+      {isReady ? <AppRouter /> : <Loader />}
       </QueryClientProvider>
     </BrowserRouter>
   );
---
+++ src/components/ui/Loader.tsx
@@ -0,0 +1,10 @@
+function Loader() {
+    return (
+        <div className="loaderWrapper">
+          <div className="spinner"></div>
+          <p className="text">Salom Hayot yuklanmoqda...</p>
+        </div>
+      );
+}
+
+export default Loader
