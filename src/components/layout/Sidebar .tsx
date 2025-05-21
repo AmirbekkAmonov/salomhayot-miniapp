a7530f3f85a715202980a255c3b0c2a13754d179
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-05-21 20:34:01 +0500

    sidebar

--- src/components/layout/Sidebar .tsx
+++ src/components/layout/Sidebar .tsx
@@ -1,7 +1,20 @@
-
-function Sidebar () {
+import { FaBars } from "react-icons/fa"
+import logo from "../../assets/images/logo.png"
+import brand from "../../assets/images/brand.png"
+import { Link } from "react-router-dom"
+function Sidebar() {
   return (
-    <div>Sidebar </div>
+    <div className="sidebar">
+      <div className="container sidebar__container">
+          <div className="sidebar__icon">
+            <FaBars />
+          </div>
+          <Link to="/" className="sidebar__logo">
+            <img src={logo} alt="logo" className="sidebar__logo-img" />
+            <img src={brand} alt="brand" className="sidebar__logo-brand" />
+          </Link>
+        </div>
+    </div>
   )
 }
 
