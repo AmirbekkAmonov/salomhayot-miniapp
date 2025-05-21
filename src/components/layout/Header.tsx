ea526d2bb30405dbbac9d3eadf91f80ce0042ca2
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-05-21 18:48:23 +0500

    herader

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,32 @@
+import { FaHome, FaThList, FaShoppingCart, FaHeart, FaUser } from 'react-icons/fa';
+import { NavLink } from "react-router-dom"
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header>
+            <div className="container header__container">
+                    <NavLink to="/" className="header__btn">
+                        <FaHome color='var(--primary-color)' className='header__btn-icon' />
+                        <span>Bosh sahifa</span>
+                    </NavLink>
+                    <NavLink to="/catalog" className="header__btn">
+                        <FaThList color='var(--primary-color)' className='header__btn-icon' />
+                        <span>Katalog</span>
+                    </NavLink>
+                    <NavLink to="/cart" className="header__btn">
+                        <FaShoppingCart color='var(--primary-color)' className='header__btn-icon' />
+                        <span>Savat</span>
+                    </NavLink>
+                    <NavLink to="/favorites" className="header__btn">
+                        <FaHeart color='var(--primary-color)' className='header__btn-icon' />
+                        <span>Sevimli</span>
+                    </NavLink>
+                    <NavLink to="/profile" className="header__btn">
+                        <FaUser color='var(--primary-color)' className='header__btn-icon' />
+                        <span>Profil</span>
+                    </NavLink>
+                </div>
+        </header>
+    )
 }
 
 export default Header
--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,17 +1,17 @@
-import { Outlet } from "react-router-dom"
-import Footer from "./Footer"
-import Header from "./Header"
-import Sidebar from "./Sidebar "
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar ";
 
 function Layout() {
   return (
-    <div>
-        <Header />
-        <Sidebar />
-        <Outlet />
-        <Footer />
+    <div className="layout">
+      <Header />
+      <Sidebar />
+      <Outlet />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
