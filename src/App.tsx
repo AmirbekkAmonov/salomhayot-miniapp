5706ac732c257c0a053a16fe7287b373095e54ab
Amirbek_Amonov <amonovamirbek1@gmail.com>
2025-05-23 16:54:38 +0500

    home page card

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
 import AppRouter from "./router";
 import Loader from "./components/ui/Loader";
+import { useThemeStore } from "./hooks/ThemeContext";
 
 const queryClient = new QueryClient();
 
 function App() {
   const [isReady, setIsReady] = useState(false);
+  const detectTheme = useThemeStore((state) => state.detectTheme);
 
   useEffect(() => {
+    detectTheme();
     const timer = setTimeout(() => setIsReady(true), 500);
     return () => clearTimeout(timer);
-  }, []);
+  }, [detectTheme]);
 
   return (
-      <BrowserRouter>
-        <QueryClientProvider client={queryClient}>
-          {isReady ? <AppRouter /> : <Loader />}
-        </QueryClientProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        {isReady ? <AppRouter /> : <Loader />}
+      </QueryClientProvider>
+    </BrowserRouter>
   );
 }
 
